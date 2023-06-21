d7d7563ecad0e6ee8af444db6f6758ce77496415
csa7mdm <cs.a7md.m@gmail.com>
2023-06-22 02:12:36 +0300

    Update payment_state.dart
    AM Edit

--- lib/Application/cubit/payment_state.dart
+++ lib/Application/cubit/payment_state.dart
@@ -1,31 +1,24 @@
-abstract class PaymentState {}
+class PaymentError {
+  String message;
+  PaymentError(this.message);
+}
+
+class PaymentSuccess {
+  String orderId;
+  PaymentSuccess(this.orderId);
+}
+
+class PaymentState {
+}
 
 class PaymentInitialState extends PaymentState {}
 
-class PaymentSuccessState extends PaymentState {}
-
 class PaymentErrorState extends PaymentState {
-  String error;
+  PaymentError error;
   PaymentErrorState(this.error);
 }
 
-class PaymentOrderIdSuccessState extends PaymentState {}
-
-class PaymentOrderIdErrorState extends PaymentState {
-  String error;
-  PaymentOrderIdErrorState(this.error);
-}
-
-class PaymentRequestTokenSuccessState extends PaymentState {}
-
-class PaymentRequestTokenErrorState extends PaymentState {
-  String error;
-  PaymentRequestTokenErrorState(this.error);
-}
-
-class PaymentRequestTokenKioskSuccessState extends PaymentState {}
-
-class PaymentRequestTokenKioskErrorState extends PaymentState {
-  String error;
-  PaymentRequestTokenKioskErrorState(this.error);
+class PaymentSuccessState extends PaymentState {
+  PaymentSuccess success;
+  PaymentSuccessState(this.success);
 }
