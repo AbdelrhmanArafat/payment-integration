79c4d78327e674132b47f7931bf3800d173de65b
AbdelrhmanArafat <abdelrhmanarafatahmed@gmail.com>
2023-06-27 03:14:13 +0300

    Deleting unneeded items

--- lib/main.dart
+++ lib/main.dart
@@ -17,18 +17,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-// void navigateTo(context, widget) => Navigator.push(
-// context ,
-// MaterialPageRoute(
-// builder: (context) => widget),
-// );
-
-// void navigateAndFinish(context, widget) => Navigator.pushAndRemoveUntil(
-// context,
-// MaterialPageRoute(
-// builder: (context) => widget,
-// ),
-// (Route<dynamic> route)=> false,
-// );
